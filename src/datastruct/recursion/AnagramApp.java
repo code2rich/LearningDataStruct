1738f401ace2bf92cd34babba4b4ac1855acdd86
zhujin97 <zhujin97@hotmail.com>
2019-11-17 20:06:15 +0800

    变位字（感觉这个很容易出题啊）

---
+++ src/datastruct/recursion/AnagramApp.java
@@ -0,0 +1,73 @@
+/**
+ * heyzen.club Inc.
+ * Copyright (c) 2018-2019 All Rights Reserved.
+ */
+package datastruct.recursion;
+
+import java.util.Scanner;
+
+/**
+ * @author Zen
+ * @version : AnagramApp.java, v 0.1 2019/11/17 19:50 Zen Exp $
+ */
+public class AnagramApp {
+    static int size ;
+    static int count ;
+    static char[] arrChar = new char[100];
+
+    public static void main(String[] args) {
+        System.out.println("请输入一个单词：");
+        Scanner sc = new Scanner(System.in);
+        if(sc.hasNext()){
+            String input = sc.next() ;
+            size = input.length();
+            count = 0;
+            for(int j = 0 ; j < size ; j++){
+                arrChar[j] = input.charAt(j);
+            }
+            doAnagram(size);
+        }
+
+    }
+
+    public static void doAnagram(int newSize) {
+        if(newSize == 1){
+            return;
+        }
+        for(int j = 0 ; j < newSize ; j++){
+            doAnagram(newSize-1);
+            if(newSize == 2) {
+                displayWord();
+            }else{
+                rotate(newSize);
+            }
+        }
+    }
+
+    public static void rotate(int newSize) {
+        int j ;
+        int position = size -newSize;
+        char temp = arrChar[position];
+        for( j = position+1 ; j < size;j++){
+            arrChar[j-1] = arrChar[j];
+        }
+        arrChar[j-1] = temp;
+    }
+    public static void displayWord(){
+        if(count < 99){
+            System.out.print(" ");
+        }
+        if(count < 9){
+            System.out.print(" ");
+        }
+        System.out.print(++count+" ");
+        for(int j = 0 ; j < size ; j++){
+            System.out.print(arrChar[j]);
+        }
+        System.out.print("  ");
+        System.out.flush();
+        if(count%6 == 0){
+            System.out.println();
+        }
+    }
+}
